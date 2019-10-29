f9cd18920b8784eb638cdcce3633009779137fce
Isale <bryanaim00@gmail.com>
2019-10-29 10:08:04 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
